c617bedc53f8c44b3ac3dcdf1c932eff47131bcd
raythurman2386 <raymondthurman5@gmail.com>
2019-10-23 22:42:37 -0400

    added logger

--- marvel-comics/src/index.js
+++ marvel-comics/src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import { Provider } from 'react-redux'
 import { reducer } from './reducers'
 import './index.css'
 import App from './App'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
   <Provider store={store}>
