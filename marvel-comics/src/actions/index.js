83b8134be625542a975c1231d254f49e7809820f
raythurman2386 <raymondthurman5@gmail.com>
2019-10-23 22:06:26 -0400

    action set up to grab comics

---
+++ marvel-comics/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { publicKey } from '../private/private'
+
+export const GET_COMICS_START = 'GET_COMICS_START'
+export const GET_COMICS_SUCCESS = 'GET_COMICS_SUCCESS'
+export const GET_COMICS_FAILED = 'GET_COMICS_FAILED'
+
+export const getMarvelComics = () => dispatch => {
+  dispatch({ type: GET_COMICS_START })
+  axios
+    .get(`https://gateway.marvel.com:443/v1/public/comics?apikey=${publicKey}`)
+    .then(res =>
+      dispatch({ type: GET_COMICS_SUCCESS, payload: res.data.results })
+    )
+    .catch(err => dispatch({ type: GET_COMICS_FAILED, payload: err }))
+}
