df8d0f4d167ae004985da942ffc376292b62a95d
raythurman2386 <raymondthurman5@gmail.com>
2019-10-23 22:06:07 -0400

    redux and thunk set up for index and app

--- marvel-comics/src/App.js
+++ marvel-comics/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getMarvelComics } from './actions'
 
 function App() {
+  useEffect(() => {}, [])
+
   return (
     <div className='App'>
       <h1>Test</h1>
--- marvel-comics/src/index.js
+++ marvel-comics/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { reducer } from './reducers'
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
