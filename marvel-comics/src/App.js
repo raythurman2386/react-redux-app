2b8c1f93307e864b3da354e75af8e35b5eb842ff
raythurman2386 <raymondthurman5@gmail.com>
2019-10-24 19:23:38 -0400

    background styles added to page

--- marvel-comics/src/App.js
+++ marvel-comics/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPopularTv } from './actions'
+import styled from 'styled-components'
 
 // Components
 import TvSeriesList from './components/TvSeriesList'
@@ -15,12 +16,24 @@ function App() {
   }, [])
 
   return (
-    <div className='App'>
-      <h1>Test</h1>
+    <AppWrapper>
+      <AppHeader>Test</AppHeader>
       {error && <h1>{error}</h1>}
       {isLoading ? <div className='spinner' /> : <TvSeriesList />}
-    </div>
+    </AppWrapper>
   )
 }
 
 export default App
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+const AppHeader = styled.header`
+  background-color: #111;
+  color: white;
+  height: 6rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
