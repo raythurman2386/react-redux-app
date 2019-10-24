ef29a5552d8fdf5f7676318ab3a2829301b1dae2
raythurman2386 <raymondthurman5@gmail.com>
2019-10-23 22:06:17 -0400

    reducer set up

--- marvel-comics/src/reducers/index.js
+++ marvel-comics/src/reducers/index.js
@@ -1,7 +1,33 @@
-export const initialState = {}
+import {
+  GET_COMICS_START,
+  GET_COMICS_SUCCESS,
+  GET_COMICS_FAILED
+} from '../actions'
+
+const initialState = {
+  comics: [],
+  isLoading: false,
+  error: ''
+}
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_COMICS_START:
+      return {
+        ...state,
+        isLoading: !state.isLoading
+      }
+    case GET_COMICS_SUCCESS:
+      return {
+        ...state,
+        comics: action.payload,
+        isLoading: !state.isLoading
+      }
+    case GET_COMICS_FAILED:
+      return {
+        ...state,
+        error: action.payload
+      }
     default:
       return state
   }
