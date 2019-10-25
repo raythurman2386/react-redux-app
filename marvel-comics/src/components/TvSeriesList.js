5addfe6e4061be0743561165a494416bd2b9d582
raythurman2386 <raymondthurman5@gmail.com>
2019-10-24 21:17:41 -0400

    shows rendering with the context api plus redux

--- marvel-comics/src/components/TvSeriesList.js
+++ marvel-comics/src/components/TvSeriesList.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import Show from './Show'
+import { ActionsContext } from '../contexts/ActionsContext'
 
 const TvSeriesList = () => {
+  const {
+    actions: { fetchTvShows }
+  } = useContext(ActionsContext)
+
+  const { isLoading, error } = useSelector(state => state)
   const series = useSelector(state => state.series)
+
+  useEffect(() => {
+    fetchTvShows()
+  }, [])
 
   return (
     <TvGrid>
+      {isLoading && <h3>Loading . . .</h3>}
+      {error && <h3>{error}</h3>}
       {series &&
         series.map((series, index) => <Show key={index} show={series} />)}
     </TvGrid>
