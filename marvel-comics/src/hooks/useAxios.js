d0e8dd6a28570e6306019c3f74a15323dd18b6af
raythurman2386 <raymondthurman5@gmail.com>
2019-10-24 20:13:19 -0400

    custom hook for action creator

--- marvel-comics/src/hooks/useAxios.js
+++ marvel-comics/src/hooks/useAxios.js
@@ -1,13 +1,27 @@
-import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
+import { publicKey } from '../private/private'
 import {
   GET_SERIES_START,
   GET_SERIES_SUCCESS,
   GET_SERIES_FAILED
 } from '../actions'
 
-const useAxios = () => dispatch => {
-  const [shows, getShows] = useState([])
+export const useAxios = () => {
+  const dispatch = useDispatch()
 
-  return [shows, getShows]
+  const fetchTvShows = useCallback(() => {
+    dispatch({ type: GET_SERIES_START })
+    axios
+      .get(`https://api.themoviedb.org/3/tv/popular?api_key=${publicKey}`)
+      .then(res =>
+        dispatch({ type: GET_SERIES_SUCCESS, payload: res.data.results })
+      )
+      .catch(err =>
+        dispatch({ type: GET_SERIES_FAILED, payload: err.response })
+      )
+  }, [dispatch])
+
+  return { fetchTvShows }
 }
