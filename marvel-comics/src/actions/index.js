8c8887074ee1951dc7f221429062916f95f54d00
raythurman2386 <raymondthurman5@gmail.com>
2019-10-24 20:10:59 -0400

    refactored actions

--- marvel-comics/src/actions/index.js
+++ marvel-comics/src/actions/index.js
@@ -1,16 +1,3 @@
-import axios from 'axios'
-import { publicKey } from '../private/private'
-
 export const GET_SERIES_START = 'GET_SERIES_START'
 export const GET_SERIES_SUCCESS = 'GET_SERIES_SUCCESS'
 export const GET_SERIES_FAILED = 'GET_SERIES_FAILED'
-
-export const getPopularTv = () => dispatch => {
-  dispatch({ type: GET_SERIES_START })
-  axios
-    .get(`https://api.themoviedb.org/3/tv/popular?api_key=${publicKey}`)
-    .then(res =>
-      dispatch({ type: GET_SERIES_SUCCESS, payload: res.data.results })
-    )
-    .catch(err => dispatch({ type: GET_SERIES_FAILED, payload: err }))
-}
